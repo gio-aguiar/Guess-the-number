d279a00c04bf806fe4533adbf1ab7c27c4754ee7
gio-aguiar <torresssgio@gmail.com>
2024-01-19 00:05:43 -0300

    guess the number

---
+++ app.js
@@ -0,0 +1,35 @@
+alert("Boas vindas ao jogo do número secreto");
+
+
+let chute = prompt('Escolha um número entre 1 e 30');
+let quantidade = 100;
+let numeroSecreto = parseInt(Math.random() * quantidade + 1);
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    if (chute > numeroSecreto) {
+        alert ("O número secreto é menor que isso");
+    } else if (chute < numeroSecreto) {
+        alert ("O número secreto é maior que isso");
+    } else { 
+        alert ("Digite um número válido") 
+    }
+
+    chute = prompt(`Escolha um número entre 1 e ${quantidade}`);
+    tentativas++;
+
+}
+
+while (isNaN(chute)) {
+    alert(`Por favor, digite um número válido entre 1 e ${quantidade}`);
+    chute = prompt(`Escolha um número entre 1 e ${quantidade}`);
+}
+
+if (tentativas > 1) {
+    alert (`Você acertou o número secreto com ${tentativas} tentativas!`)
+}
+else {
+    alert ("Você descobriu o número secreto logo na primeira tentativa! ")
+}
+
+
